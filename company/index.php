3ad371140ef4ebe4585ba439959b21c7908dfafb
bpoc-mark <markariel.maata@bpoc.co.jp>
2022-02-22 15:04:17 +0800

    added company page pc and sp view

---
+++ company/index.php
@@ -0,0 +1,381 @@
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/config.php"); ?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/tag_manager.php"); ?>
+  <meta charset="UTF-8" />
+  <meta name="robots" content="index,follow">
+  <link rel="canonical" href="<?php echo CANONICAL ?>">
+  <meta name="keywords" content="<?php echo KEY_WORD ?>">
+  <meta name="description" content="<?php echo DESCRIPTION ?>">
+  <meta property="og:title" content="<?php echo SITE_NAME ?>">
+  <meta property="og:type" content="article" /> <!-- 下層では「article」にしてください -->
+  <meta property="og:description" content="<?php echo DESCRIPTION ?>" />
+  <meta property="og:url" content="<?php echo CANONICAL ?>" />
+
+  <title>企業紹介 | <?php echo SITE_NAME ?></title>
+
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/header_include.php"); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/ga.php"); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/header_tag.php"); ?>
+</head>
+
+<body id="Company">
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/content_tag.php"); ?>
+  <div id="AllBox">
+    <header>
+      <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/header_box.php"); ?>
+    </header>
+
+    <main>
+      <section class="p-header">
+        <div class="p-header--wrap">
+          <p class="p-header--sm">企業紹介</p>
+          <h1 class="p-header--lg">エスビー鉄鋼株式会社</h1>
+        </div>
+      </section>
+      <section class="sect-1">
+        <div class="container">
+          <div class="content">
+            <div class="txt-row">
+              <div class="col">
+                <img src="/images/company/img_1.jpg" alt="SBTEKKO">
+              </div>
+              <div class="col">
+                <p>エスビー鉄鋼では「最後まで働きたい会社作り」を究極の目標に掲げ、社員の喜びと業績安定に取り組み、地域貢献を目指しています。</p>
+                <span>＃ものづくり（製造） #製造</span>
+              </div>
+            </div>
+            <div class="job-row">
+              <div class="col">
+                <h3 class="ratings-ttl">求人の特徴</h3>
+                <div class="ratings">
+                  <div class="row">
+                    <div class="col">
+                      <p>雰囲気</p>
+                    </div>
+                    <div class="col">
+                      <div class="star-wrap">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon-wh.svg" alt="">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <p>働きがい</p>
+                    </div>
+                    <div class="col">
+                      <div class="star-wrap">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon-wh.svg" alt="">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <p>やりがい</p>
+                    </div>
+                    <div class="col">
+                      <div class="star-wrap">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon-wh.svg" alt="">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <p>自己成長度</p>
+                    </div>
+                    <div class="col">
+                      <div class="star-wrap">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <p>残業時間</p>
+                    </div>
+                    <div class="col">
+                      <div class="star-wrap">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon-wh.svg" alt="">
+                        <img src="/images/common/star-icon-wh.svg" alt="">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <p>休日数</p>
+                    </div>
+                    <div class="col">
+                      <div class="star-wrap">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon-wh.svg" alt="">
+                        <img src="/images/common/star-icon-wh.svg" alt="">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <p>通勤</p>
+                    </div>
+                    <div class="col">
+                      <div class="star-wrap">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon-wh.svg" alt="">
+                        <img src="/images/common/star-icon-wh.svg" alt="">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <p>食事補助</p>
+                    </div>
+                    <div class="col">
+                      <div class="star-wrap">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon-wh.svg" alt="">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <p>住宅補助</p>
+                    </div>
+                    <div class="col">
+                      <div class="star-wrap">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <p>認定、資格取得</p>
+                    </div>
+                    <div class="col">
+                      <div class="star-wrap">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon.svg" alt="">
+                        <img src="/images/common/star-icon-wh.svg" alt="">
+                        <img src="/images/common/star-icon-wh.svg" alt="">
+                        <img src="/images/common/star-icon-wh.svg" alt="">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                <div class="img-row">
+                  <div class="img-row--col">
+                    <img src="/images/company/img_2.jpg" alt="">
+                  </div>
+                  <div class="img-row--col pc">
+                    <img src="/images/company/img_3.jpg" alt="">
+                  </div>
+                  <div class="img-row--col pc">
+                    <img src="/images/company/img_4.jpg" alt="">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="sect-2">
+        <div class="container">
+          <div class="content">
+            <h3 class="ttl">動画でチェック</h3> 
+            <p class="yt-wrap">
+              <iframe width="882" height="498" src="https://www.youtube.com/embed/UB4DqyTL5ho?rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+            </p>
+          </div>
+        </div>
+      </section>
+      <section class="sect-3">
+        <div class="container">
+          <div class="content">
+            <h3 class="ttl">フリーエリアタイトルが入ります</h3>
+            <div class="desc-wrap">
+              <p class="desc">
+                当社は人材育成の柱として<br>
+                ①資格取得支援制度と資格手当支給<br>
+                ②新人にはOJTによる3年間マンツーマン指導<br>
+                ③専属契約コンサルによる人材育成研修を行っています。
+              </p>
+              <p class="desc">
+                福利厚生面では<br>
+                ①社員旅行の実施<br>
+                ②グループ会社合同運動会の開催<br>
+                ③忘年会の開催等を行い社員コミュニケーションを図っています。
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="sect-4">
+        <div class="container">
+          <div class="content">
+            <h3 class="ttl">仕事体験に応募する</h3>
+            <div class="btn-wrap">
+              <a href="tel:0537-48-2310">電話で応募 : 0537-48-2310</a>
+              <a href="">メールで応募</a>
+            </div>
+            <table class="company-table">
+              <tr>
+                <th>求人職種</th>
+                <td>製造職</td>
+              </tr>
+              <tr>
+                <th>求人予定</th>
+                <td>2名（大卒、短大、専門、高卒、中途）</td>
+              </tr>
+              <tr>
+                <th>採用方法</th>
+                <td>履歴書、筆記試験、適性検査、面接、役員面接</td>
+              </tr>
+              <tr>
+                <th>仕事内容</th>
+                <td>鉄骨建築物の設計製作加工会社です。技術向上と高品質制作を基本方針とし、設計、加工、組立、溶接、塗装から運搬までの鉄骨製作工程を社員一丸となって行っています。</td>
+              </tr>
+              <tr>
+                <th>勤務地</th>
+                <td>静岡県掛川市大渕8158-1</td>
+              </tr>
+              <tr>
+                <th>最寄り駅</th>
+                <td>電車 ： 袋井駅　　バス ： 静鉄 横須賀車庫</td>
+              </tr>
+              <tr>
+                <th>従業員数</th>
+                <td>50人以下</td>
+              </tr>
+              <tr>
+                <th>給与</th>
+                <td>
+                  高校卒 180,000円 / 短大・専門卒 200,000円 / 大学卒 215,000円<br>
+                  昇給制度 : 有(前年度実績：7,500円)<br>
+                  賞与 : 有(新規学卒者の前年度実績：2.67ヶ月)<br>
+                  資格手当 : 有
+                </td>
+              </tr>
+              <tr>
+                <th>福利厚生・サポート</th>
+                <td>
+                  食事補助 : 有(月額5,000円補助)<br>
+                  住宅補助 : 有（32歳まで家賃の80％補助）<br>
+                  マイカー通勤補助 : 有（月額2,000～20,000円）
+                </td>
+              </tr>
+              <tr>
+                <th>勤務・残業時間</th>
+                <td>
+                  勤務時間 : 8：00～17：05（休憩時間 1時間20分）<br>
+                  残業 : 月22時間程度
+                </td>
+              </tr>
+              <tr>
+                <th>休日数</th>
+                <td>110日以上</td>
+              </tr>
+              <tr>
+                <th>転勤</th>
+                <td>なし</td>
+              </tr>
+              <tr>
+                <th>その他<br>資格などの採用条件</th>
+                <td>普通自動車運転免許</td>
+              </tr>
+              <tr>
+                <th>URL</th>
+                <td><a href="https://www.sb-tekko.jp" target="_blank">https://www.sb-tekko.jp</a></td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </section>
+      <section class="sect-5">
+        <div class="container">
+          <div class="content">
+            <h3 class="ttl">会社概要</h3> 
+            <div class="row">
+              <div class="col">
+                <table>
+                  <tr>
+                    <th>会社名</th>
+                    <td>エスビー鉄鋼株式会社</td>
+                  </tr>
+                  <tr>
+                    <th>創業</th>
+                    <td>昭和44年2月1日</td>
+                  </tr>
+                  <tr>
+                    <th>代表者</th>
+                    <td>海津 宗之</td>
+                  </tr>
+                </table>
+              </div>
+              <div class="col">
+                <table>
+                    <tr>
+                      <th>所在地</th>
+                      <td>静岡県掛川市大渕8158-1</td>
+                    </tr>
+                    <tr>
+                      <th>資本金</th>
+                      <td>10,000,000円</td>
+                    </tr>
+                    <tr>
+                      <th>事業内容</th>
+                      <td>製造</td>
+                    </tr>
+                  </table>
+              </div>
+            </div>
+            <h3 class="ttl-2">仕事体験に応募する</h3>
+            <div class="btn-wrap">
+              <a href="tel:0537-48-2310">電話で応募 : 0537-48-2310</a>
+              <a href="">メールで応募</a>
+            </div>
+            <a href="" class="back-to-list">一覧へ戻る</a>
+          </div>
+        </div>
+      </section>
+    </main>
+
+    <footer>
+      <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/footer_box.php"); ?>
+    </footer>
+  </div><!-- //AllBox -->
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/footer_tag.php"); ?>
+</body>
+
+</html>
